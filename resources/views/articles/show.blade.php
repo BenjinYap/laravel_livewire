c35a2506267d38f17d002f618553e7f1ff23ab0d
Benjin Yap <benjyap7+github@gmail.com>
2024-06-11 15:17:34 -0400

    * Updated styles for article page.

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -1,5 +1,23 @@
 <x-layout>
-    <h1>{{$article->title}}</h1>
-    <h2>{{$article->created_at}}</h2>
-    <h2>{{$article->updated_at}}</h2>
+    <section class="mb-4">
+        <h1 class="text-3xl font-bold">{{$article->title}}</h1>
+        <div class="flex gap-2 text-gray-400 text-sm">
+            <div class="flex items-center gap-0.5">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                </svg>
+                <span>{{$article->created_at}}</span>
+            </div>
+            <div class="flex items-center gap-0.5">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                </svg>
+                <span>Benjin</span>
+            </div>
+        </div>
+    </section>
+
+    <section>
+        hello
+    </section>
 </x-layout>
